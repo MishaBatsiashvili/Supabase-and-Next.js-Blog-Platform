cfb8efece4ce94d0c06986240f5cf50809471412
Misha Batsiashvili <mishabatsiashvili@yahoo.com>
2024-11-19 08:14:23 +0400

    fix

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -4,3 +4,5 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next';
 const handler = startServerAndCreateNextHandler(apolloServer);
 
 export { handler as GET, handler as POST };
+
+export const dynamic = "force-dynamic";
