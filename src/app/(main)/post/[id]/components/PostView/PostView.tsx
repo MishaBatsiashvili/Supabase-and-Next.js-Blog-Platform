b75d1a93d5aa9d44e943f3e6e7ebbb4662cef6f2
Misha Batsiashvili <mishabatsiashvili@yahoo.com>
2024-11-19 19:58:02 +0400

    added padding to post content

--- src/app/(main)/post/[id]/components/PostView/PostView.tsx
+++ src/app/(main)/post/[id]/components/PostView/PostView.tsx
@@ -40,7 +40,7 @@ const PostView: React.FC<{
       <div className="mb-24 mt-6">
         <h1 className="text-center text-2xl font-bold">{post.title}</h1>
         <p
-          className="mt-6"
+          className="mt-6 pb-16"
           dangerouslySetInnerHTML={renderContentWithLineBreaks(post.content)}
         />
       </div>
