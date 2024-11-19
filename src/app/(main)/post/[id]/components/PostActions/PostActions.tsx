957756edfc2059a42afbd123547e129e5a7a5b98
Misha Batsiashvili <mishabatsiashvili@yahoo.com>
2024-11-19 09:57:13 +0400

    Post fix

--- src/app/(main)/post/[id]/components/PostActions/PostActions.tsx
+++ src/app/(main)/post/[id]/components/PostActions/PostActions.tsx
@@ -4,10 +4,16 @@ import React from 'react'
 import DeleteAction from './DeleteAction'
 import { GetPostQuery } from '@/__generated__/graphql'
 import EditAction from './EditAction/EditAction'
+import { useUser } from '@/contexts/UserContext/UserContext'
 
 const PostActions: React.FC<{
   post: Exclude<GetPostQuery['post'], null | undefined>
 }> = ({ post }) => {
+
+  const { user } = useUser()
+  if(!user || user?.id !== post.user_id){
+    return <></>
+  }
 
   return (
     <div className="mt-3 flex justify-center">
